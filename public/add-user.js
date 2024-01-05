b9e5b9c3904c80adffc5f141f4c39a0adeb1adf1
Bryan Blanchot <Bryan21B@users.noreply.github.com>
2024-01-05 19:49:32 +0100

    Add frontend logic to create users

--- public/add-user.js
+++ public/add-user.js
@@ -6,13 +6,16 @@ submitButton.addEventListener("click", () => {
     const lastName = document.getElementById("lastname").value
     const email = document.getElementById("email").value
 
-    fetch(`/api/quotes?quote=${quote}&person=${person}`, {
+    fetch(`/api/users?firstName=${firstName}&lastName=${lastName}`, {
         method: "POST",
     })
         .then((response) => response.json())
         .then(({ user }) => {
             const newUser = document.createElement("div")
-            newUser.innerHTML = `<h3>Congrats, you've created a new user!</h3> `
+            newUser.innerHTML = `<h3>Congrats, you've created a new user!</h3>
+    <div class="user-firstName">${user.firstName}</div>
+    <div class="user-lastName">- ${user.lastName}</div>
+    <p>Go to the <a href="index.html">home page</a> to request and view all quotes.</p> `
             newUserContainer.appendChild(newUser)
         })
 })
