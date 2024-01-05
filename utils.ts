b5b0413c4ea2e453b4eb034d370c529dcebb7cb1
Bryan Blanchot <Bryan21B@users.noreply.github.com>
2024-01-05 19:32:39 +0100

    Refactor findNextID function to remove mention of quotes

--- utils.ts
+++ utils.ts
@@ -8,10 +8,10 @@ export const filterQuotesByPerson = (quotesArray: Quote[], personName: any): Quo
     return quotesArray.filter((quote) => quote.person === personName)
 }
 
-export const findNextAvailableId = (quotesArray: Quote[]): number => {
+export const findNextAvailableId = (array: Quote[]): number => {
     // Use the map function to create an array of ids,
     // then use the spread operator to apply Math.max to it
-    const maxId = Math.max(...quotesArray.map((quote) => quote.id))
+    const maxId = Math.max(...array.map((item) => item.id))
 
     // The next available id will be maxId + 1
     return maxId + 1
